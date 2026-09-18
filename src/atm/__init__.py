5e9f3ea39e9026a5aa83b17246e7dc5288654d87
lyfuci <lyfuci11@gmail.com>
2026-09-18 10:14:49 +0800

    chore: bump version to 0.11.0

--- src/atm/__init__.py
+++ src/atm/__init__.py
@@ -9,6 +9,6 @@
 
 from .model import IndexStats, SessionEntry, SessionIndex, Source
 
-__version__ = "0.10.4"
+__version__ = "0.11.0"
 
 __all__ = ["IndexStats", "SessionEntry", "SessionIndex", "Source", "__version__"]
